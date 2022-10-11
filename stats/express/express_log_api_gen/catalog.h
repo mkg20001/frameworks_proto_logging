d71bac95c0e9f2ee74cca9754cfbe54b761ca463
Vova Sharaienko <sharaienko@google.com>
2022-10-11 03:00:32 +0000

    [TeX] Introduced Telemetry Express Logging Library
    - supports C++ & Java codegen
    - build time metric catalog validation
    
    Bug: 247803110
    Test: express_logging_native_client
    Test: atest ExpressLogTests
    Change-Id: Ieb889028a353474e99f851584deca05e47856985

---
+++ stats/express/express_log_api_gen/catalog.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2022, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <google/protobuf/message.h>
+#include <stdint.h>
+
+#include <string>
+#include <unordered_map>
+
+#include "frameworks/proto_logging/stats/express/express_config.pb.h"
+#include "frameworks/proto_logging/stats/express/express_log_api_gen/catalog.pb.h"
+
+namespace android {
+namespace express {
+
+/**
+ * @brief Recursively parser folder, locates all cfg files and reads ExpressMetric configs
+ */
+bool readCatalog(const char* path, std::unordered_map<std::string, ExpressMetric>& metrics);
+
+typedef std::unordered_map<std::string, MetricInfo> MetricInfoMap;
+
+bool generateMetricsIds(const std::unordered_map<std::string, ExpressMetric>& metrics,
+                        MetricInfoMap& metricsIds);
+
+}  // namespace express
+}  // namespace android
