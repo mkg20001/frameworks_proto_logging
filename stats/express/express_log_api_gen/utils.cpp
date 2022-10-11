c3cc191644cdd7297765c5e999afabf88f9ecd19
Vova Sharaienko <sharaienko@google.com>
2022-10-11 03:00:32 +0000

    [TeX] Introduced Telemetry Express Logging Library
    - supports C++ & Java
    - build time metric catalog validation
    
    Bug: 247803110
    Test: m libexpresslog
    Change-Id: Ieb889028a353474e99f851584deca05e47856985

---
+++ stats/express/express_log_api_gen/utils.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2022, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "utils.h"
+
+#include <stdio.h>
+
+#include <string>
+#include <vector>
+
+namespace android {
+namespace express {
+
+using namespace std;
+
+vector<string> split(const string& s, const string& delimiters) {
+    vector<string> result;
+
+    size_t base = 0;
+    size_t found;
+    while (true) {
+        found = s.find_first_of(delimiters, base);
+        result.push_back(s.substr(base, found - base));
+        if (found == s.npos) break;
+        base = found + 1;
+    }
+
+    return result;
+}
+
+}  // namespace express
+}  // namespace android
